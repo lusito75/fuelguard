e3beac2615148f5a85d04aa745921af7209a39e9
Paulo Lourenco <paulo@lourenco.net.au>
2014-06-10 22:45:42 +1000

    initial openshift commit

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDIS
      packages.append('django-redis-cache')
      packages.append('hiredis')
 
-setup(name='YourAppName',
+setup(name='FuelGuard',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Paulo Lourenco',
+      author_email='paulo@lourenco.net.au',
       url='https://pypi.python.org/pypi',
       install_requires=packages,
 )
