8974141a66e5cf57096b595abbf1f17a850ebe81
paulo@lourenco.net.au <>
2014-06-13 06:59:02 +0000

    Force debug=true to see if static files will be served

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -33,9 +33,9 @@ if ON_OPENSHIFT:
 SECRET_KEY = use_keys['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if ON_OPENSHIFT:
-     DEBUG = False
-else:
+#if ON_OPENSHIFT:
+#     DEBUG = False
+#else:
      DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
